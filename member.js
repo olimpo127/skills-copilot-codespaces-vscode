27aa3e2341ee8c9b39b4fcdb503f93e77ae65fe0
Felipe Ojeda Iturriaga <92526027+olimpo127@users.noreply.github.com>
2023-11-03 13:18:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var member = document.getElementById("member");
+    var member = document.getElementById("member");
+    var member = document.getElementById("member");
+    var member
+}
